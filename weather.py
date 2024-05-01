b8adb97480fc0e3f32a459ad30fe215668691238
Syeda Yasmin <44278302+syasminire23@users.noreply.github.com>
2024-05-01 17:23:00 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 
-def get_current_weather(city="Kansas City"):
+def get_current_weather(city="London"):
 
     request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
